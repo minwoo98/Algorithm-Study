58ea57cf17088eb8a1f3dc6009d7012ca2273aa5
minwoo98 <cola31929395@gmail.com>
2022-01-22 23:04:05 +0900

    linked_list v3

---
+++ Algorithm basic 1/브루트 포스/사탕 게임.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std; 
+
+int check(vector<string> &a)
+{
+	int n = a.size();
+	int ans = 1;
+	for(int i=0; i<n; i++) //¿À¸¥ÂÊ  Å½»ö 
+	{
+		int cnt = 1;
+		for(int j=1; j<n; j++)
+		{
+			if(a[i][j] == a[i][j])
+			{
+				cnt++;
+			}
+			else
+			{
+				cnt = 1;
+			}
+			if(ans < cnt) ans = cnt;
+		}
+		cnt = 1;
+		for(int j=1; j<n; j++)
+		{
+			if(a[j][i] == a[j-1][i])
+			{
+				cnt++;
+			}
+			else
+			{
+				cnt = 1;
+			}
+			if(ans < cnt) ans = cnt;
+		}
+	}
+	return ans;	
+}
+
+int main(void)
+{
+	int n;
+	cin >> n;
+	vector<string> a(n);
+	for(int i=0; i<n; i++)
+	{
+		cin >> a[i];
+	}
+	
+	int ans = 0;
+	for(int i=0; i<n; i++)
+	{
+		for(int j=0; j<n; j++)
+		{
+			if(j+1<n)
+			{
+				swap(a[i][j], a[i][j+1]);
+				int temp = check(a);
+				if(ans < temp) ans = temp;
+				swap(a[i][j], a[i][j+1]);
+				
+			}
+			if(i+1<n)
+			{
+				swap(a[i][j], a[i+1][j]);
+				int temp = check(a);
+				if(ans < temp) ans = temp;
+				swap(a[i][j], a[i+1][j]);
+			}
+		}
+	}
+	cout << ans << '\n';
+	return 0;	
+}
