d63c69f61ac4c1c75254f12fbee58a450ac6eb45
minwoo98 <cola31929395@gmail.com>
2022-05-11 21:36:50 +0900

    배열 인덱스로 트리 노드 생성, 자식 없으면 -1넣어서 재귀를 통해 트리 순회(재귀 return 조건 x==-1

---
+++ Algorithm basic/트리1/트리 순회.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+int n;
+struct Node
+{
+	int left;
+	int right;
+};
+
+Node a[27];
+
+void preorder(int x)
+{
+	if(x==-1)	return;
+	
+	cout << (char)(x+'A');
+	preorder(a[x].left);
+	preorder(a[x].right);
+}
+void inorder(int x)
+{
+	if(x==-1)	return;
+	
+	inorder(a[x].left);
+	cout << (char)(x+'A');
+	inorder(a[x].right);
+
+}
+void postorder(int x)
+{
+	if(x==-1)	return;
+	
+	postorder(a[x].left);
+	postorder(a[x].right);
+	cout << (char)(x+'A');
+}
+	
+int main(void)
+{
+	cin >> n;
+	
+	for(int i=1; i<=n; i++)
+	{
+		char x,y,z;
+		cin >> x >> y >> z;
+		
+		x = x-'A';
+		if(y=='.')
+		{
+			a[x].left = -1;
+		}
+		else
+		{
+			a[x].left = y-'A';
+		}
+		if(z=='.')
+		{
+			a[x].right = -1;
+		}
+		else
+		{
+			a[x].right = z-'A';
+		}
+	}
+	
+	preorder(0);
+	cout << '\n';
+	
+	inorder(0);
+	cout << '\n';
+	
+	postorder(0);
+	cout << '\n';
+	
+	return 0;	
+}
